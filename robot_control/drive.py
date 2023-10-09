a36bfe5b91c5bca3275550724050d1113e3d11ef
MartinSimengard <simengardmartin@gmail.com>
2023-10-09 11:32:03 +0200

    Structuring

---
+++ robot_control/drive.py
@@ -0,0 +1,17 @@
+import sys
+import time
+
+sys.path.append('/dev/sphero-sdk-raspberrypi-python')
+
+#from sphero_sdk import SpheroRvrObserver
+
+#rvr = SpheroRvrObserver()
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd dev/dependencies  # change directory to the dependencies folder
+git clone https://github.com/sphero-inc/sphero-sdk-raspberrypi-python.git  # clone the the RVR SDK
+
+
+
