38b11d58e8180681a8522b0477d104391dd83d74
MartinSimengard <simengardmartin@gmail.com>
2023-10-12 17:09:09 +0200

    Drive in circle

---
+++ examples/drive_in_circle.py
@@ -0,0 +1,40 @@
+import sys
+import time
+
+
+sys.path.append('../../dependencies/sphero-sdk-raspberrypi-python')
+from sphero_sdk import SpheroRvrObserver
+
+
+rvr = SpheroRvrObserver()
+
+prev_time = time.time()
+
+def main():
+    rvr.wake()
+    time.sleep(2)
+    
+    rvr.drive_control.reset_heading()
+    
+    try:
+        deg_per_second = 90
+        head = 0
+        while True:
+            dt = time.time() - prev_time
+            prev_time = time.time()
+
+            head += deg_per_second * dt/1000
+            head = head % 360
+            
+            rvr.drive_control.drive_with_heading(64, head, None)
+            time.sleep(0.1)
+
+    
+    except KeyboardInterrupt:
+        print('\nProgram terminated with keyboard interrupt.')
+
+    finally:
+        rvr.close()
+
+if __name__ == '__main__':
+    main()
