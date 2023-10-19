7ca957f987101de3a9d79b990675de40ef5a1486
MartinSimengard <simengardmartin@gmail.com>
2023-10-19 14:57:28 +0200

    gst

--- scripts/gst-stream.sh
+++ scripts/gst-stream.sh
@@ -1,2 +1,3 @@
 #raspivid -t 0 -h 480 -w 640 -fps 25 -hf -b 1000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! udpsink host=10.25.46.36 port=5000
-raspivid -t 0 -h 480 -w 640 -fps 25 -hf -b 1000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! tcpserversink host=10.25.46.36 port=5000
+#raspivid -t 0 -h 480 -w 640 -fps 25 -hf -b 1000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! tcpserversink host=10.25.46.36 port=5000
+raspivid -t 0 -h 480 -w 640 -fps 10 -hf -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=10.25.46.36 port=5000
