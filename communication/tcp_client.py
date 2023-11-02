848a53b7c1cde7966385a8afb58aab3e312004e3
elias.woie <elias.woie@kongsberg.com>
2023-11-02 09:14:41 +0100

    Set up camera feed

--- communication/tcp_client.py
+++ communication/tcp_client.py
@@ -1,30 +1,75 @@
 import socket
+import cv2
+import numpy as np
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+from protobuf.my_messages_pb2 import VideoFeed, Instruction  # Generated from protoc
+import time
 
+# Initialize socket for UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_address = ('127.0.0.1', 8080)
+MAX_UDP_PACKET_SIZE = 65507  # Maximum size of a UDP packet
 
-class tcp_client:
+# Initialize the Raspberry Pi camera
+camera = PiCamera()
+camera.resolution = (640, 480)  # Set your desired resolution
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=camera.resolution)
 
-    def __init__(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Allow the camera to warmup
+time.sleep(0.1)
 
-    def connect(self, host, port):
-        self.socket.connect((host, port))
+try:
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        image = frame.array  # Grab the numpy array representing the image
 
-    def send(self, data):
-        msg = f"{data}".encode("utf-8")
-        msg_size = len(msg)
+        # Your existing code for processing and sending the image
+        scale_factor = 1.0
+        while True:
+            # Resize frame with the current scaling factor
+            dim = (int(image.shape[1] * scale_factor), int(image.shape[0] * scale_factor))
+            resized_frame = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+            # Encode frame to JPEG format
+            is_success, buffer = cv2.imencode(".jpg", resized_frame)
 
-        self.socket.send(msg_size.to_bytes(4, byteorder="little"))
-        self.socket.send(msg)
+            if not is_success:
+                print("Failed to encode image!")
+                break
 
-    def recv(self):
-        msg_size = int.from_bytes(self.socket.recv(4), byteorder="little")
-        data = self.socket.recv(msg_size).decode("utf-8")
+            # Serialize frame data to protobuf
+            video_feed = VideoFeed()
+            video_feed.messageFeed = buffer.tobytes()
+            serialized_video_feed = video_feed.SerializeToString()
 
-        return data
+            # Check if the serialized data fits in one UDP packet
+            if len(serialized_video_feed) <= MAX_UDP_PACKET_SIZE:
+                break
+            scale_factor -= 0.1  # Reduce scale factor
 
+            if scale_factor <= 0.1:  # Prevent too small frames
+                print("Frame too large to fit into UDP packet even after reducing resolution.")
+                break
 
-if __name__ == "__main__":
-    client = tcp_client()
-    client.connect("localhost", 9090)
-    client.send("Hello World!")
-    print(client.recv())
+        # Send serialized video feed to the server
+        sock.sendto(serialized_video_feed, server_address)
+
+        # Receive instruction from server
+        serialized_instruction, _ = sock.recvfrom(MAX_UDP_PACKET_SIZE)
+        instruction = Instruction()
+        instruction.ParseFromString(serialized_instruction)
+
+        # Print received instruction - execute it if applicable
+        print("Received instruction:", instruction.messageInstruction)
+
+        # Clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+finally:
+    # Release resources
+    camera.close()
+    cv2.destroyAllWindows()
+    sock.close()
