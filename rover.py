19a50c1c60900a677e0799a9e40fc8fce42a8768
elias.woie <elias.woie@kongsberg.com>
2023-11-02 10:22:43 +0100

    Adjusting camera feed that is sent over udp

--- rover.py
+++ rover.py
@@ -15,11 +15,18 @@ def parse_args():
 #def main(args):
 #    tcp_client = tcp_client()
 
-def mainCamera():
-    streamer = PiCamStreamer(server_address=('192.168.1.10', 8080), resolution=(640, 480), framerate=30)
+def mainCamera(args):
+    streamer = PiCamStreamer(server_address=(args.ip, 8080), resolution=(640, 480), framerate=30)
     streamer.run()
+
+def comms_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('ip', type=str, help='IP address for the RPI to stream to.')
+    args = parser.parse_args()
+    return args
 
 if __name__ == "__main__":
     #args = parse_args()
     #main(args)
-    mainCamera()
+    args = comms_args()
+    mainCamera(args)
