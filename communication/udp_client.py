2e5601d90f5820482ef4a22dcc37f228beea9519
elias.woie <elias.woie@kongsberg.com>
2023-11-02 10:03:55 +0100

    Adjusting camera feed that is sent over udp

--- communication/udp_client.py
+++ communication/udp_client.py
@@ -1,8 +1,8 @@
+import io
 import socket
-import time
 from picamera import PiCamera
 from picamera.array import PiRGBArray
-from protobuf.my_messages_pb2 import VideoFeed, Instruction  # Generated from protoc
+from protobuf.my_messages_pb2 import VideoFeed, Instruction
 
 class PiCamStreamer:
     def __init__(self, server_address=('127.0.0.1', 8080), resolution=(640, 480), framerate=32):
@@ -16,49 +16,50 @@ class PiCamStreamer:
         self.camera = PiCamera()
         self.camera.resolution = self.resolution
         self.camera.framerate = self.framerate
-        self.rawCapture = PiRGBArray(self.camera, size=self.camera.resolution)
-        time.sleep(0.1)  # Allow the camera to warm up
+
+        # Warm up the camera
+        self.camera.start_preview()
+        time.sleep(2)
+        self.camera.stop_preview()
 
     def stream_video(self):
-        try:
-            for frame in self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True):
-                image = frame.array  # Grab the numpy array representing the image
-                # Convert image to bytes
-                image_bytes = image.tobytes()
+        stream = io.BytesIO()
+        for _ in self.camera.capture_continuous(stream, format='jpeg', use_video_port=True):
+            # Truncate the stream to the current length (this is necessary to prepare it for the next frame)
+            stream.truncate()
+            stream.seek(0)
 
-                # Serialize frame data to protobuf
+            image_bytes = stream.getvalue()
+            if len(image_bytes) <= self.MAX_UDP_PACKET_SIZE:
                 video_feed = VideoFeed()
                 video_feed.messageFeed = image_bytes
                 serialized_video_feed = video_feed.SerializeToString()
 
-                # Check if the serialized data fits in one UDP packet
-                if len(serialized_video_feed) > self.MAX_UDP_PACKET_SIZE:
-                    print("Serialized data too large for UDP packet.")
-                    continue
-
-                # Send serialized video feed to the server
                 self.sock.sendto(serialized_video_feed, self.server_address)
 
-                # Receive instruction from server
                 serialized_instruction, _ = self.sock.recvfrom(self.MAX_UDP_PACKET_SIZE)
                 instruction = Instruction()
                 instruction.ParseFromString(serialized_instruction)
-
-                # Print received instruction - execute it if applicable
                 print("Received instruction:", instruction.messageInstruction)
+            else:
+                print("Image too large to send over UDP")
 
-                # Clear the stream in preparation for the next frame
-                self.rawCapture.truncate(0)
-
-        finally:
-            # Release resources
-            self.camera.close()
-            self.sock.close()
+            # Reset the stream for the next capture
+            stream.seek(0)
+            stream.truncate()
 
     def run(self):
         self.stream_video()
 
-# If you want the script to start streaming as soon as it is run, include this at the end of your file
+    def close(self):
+        self.camera.close()
+        self.sock.close()
+
 if __name__ == "__main__":
     streamer = PiCamStreamer(server_address=('127.0.0.1', 8080))
-    streamer.run()
+    try:
+        streamer.run()
+    except KeyboardInterrupt:
+        print("Stopping streamer")
+    finally:
+        streamer.close()
