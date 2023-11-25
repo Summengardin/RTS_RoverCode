2e59a37f0e1b3fc8a03fb23ce2074a9ce163d399
MartinSimengard <simengardmartin@gmail.com>
2023-11-25 15:27:05 +0100

    read dist

--- examples/read_dist_sensor.py
+++ examples/read_dist_sensor.py
@@ -1,6 +1,6 @@
 import smbus
 import qwiic_vl53l1x as qwiic_dist
-import qwiic_tca9548 as qwiic_mux
+import qwiic_tca9548a as qwiic_mux
 import time
 
 
@@ -12,15 +12,20 @@ bus_number = 1  # 1 for Rasp Pi rev 2 and 3
 bus = smbus.SMBus(bus_number)
 mux_address = 0x70  # Replace with your mux's I2C address
 
-# Function to set the channel on the mux
-def set_mux_channel(channel):
-    bus.write_byte(mux_address, 1 << channel)
+if Mux.begin():
+    print("TCA9548A initialized.")
+else:
+    print("TCA9548A initialization failed!")
+    exit(1)
+	
+# Enable channel 3 and 7
+Mux.enable_channels(3)
+Mux.enable_channels(7)
 
-set_mux_channel(0)
+
 if (Tof_front.sensor_init() == None):					 # Begin returns 0 on a good init
 	print("Sensor in front online!\n")
 
-set_mux_channel(1)
 if (Tof_back.sensor_init() == None):					 # Begin returns 0 on a good init
 	print("Sensor in back online!\n")
 
@@ -29,7 +34,6 @@ if (Tof_back.sensor_init() == None):					 # Begin returns 0 on a good init
 
 while(True):
 	try:
-		set_mux_channel(0)
 		Tof_front.start_ranging()						 # Write configuration bytes to initiate measurement
 		time.sleep(.005)
 		distance = Tof_front.get_distance()	 # Get the result of the measurement from the sensor
@@ -41,7 +45,6 @@ while(True):
 
 		print("Distance front(mm): %s Distance front(ft): %s" % (distance, distanceFeet))
 
-		set_mux_channel(1)
 		Tof_back.start_ranging()						 # Write configuration bytes to initiate measurement
 		time.sleep(.005)
 		distance = Tof_back.get_distance()	 # Get the result of the measurement from the sensor
