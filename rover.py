3fb9b0e2b9c47c3ee143604b61cc4a107b94cb15
MartinSimengard <simengardmartin@gmail.com>
2023-12-07 13:10:48 +0100

    thread count

--- rover.py
+++ rover.py
@@ -21,6 +21,7 @@ def commsArgs():
 
 
 if __name__ == "__main__":
+    print(threading.active_count())
     ip = sys.argv[1] if len(sys.argv) > 1 else "10.22.192.34"
     # args = parse_args()
     # main(args)
@@ -40,8 +41,10 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("Exiting...")
 
+    print(threading.active_count())
     stream.stop()
     stream_thread.join()
+    print(threading.active_count())
     controller.stop()
     controller_thread.join()
     
