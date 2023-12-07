ead268d6870065e3732d037cf6a8927a99815bc1
MartinSimengard <simengardmartin@gmail.com>
2023-12-07 10:17:38 +0100

    river remove obligue ip

--- rover.py
+++ rover.py
@@ -1,13 +1,15 @@
 import argparse
 import threading
 import time
+import sys
 
 from communication.udp_client import PiCamStreamer
 from rover_control import control
 
 
-def mainCamera(args):
-    streamer = PiCamStreamer(server_address=(args.ip, 8080), resolution=(320, 240), framerate=30)
+def mainCamera(ip):
+    streamer = PiCamStreamer(server_address=(ip, 8080), resolution=(320, 240), framerate=30)
+    print("Streaming to IP address:", ip)
     streamer.run()
 
 
@@ -19,21 +21,17 @@ def commsArgs():
 
 
 if __name__ == "__main__":
-    args = commsArgs()
-    if args.ip == '':
-        args.ip = '10.22.192.34'
+    ip = sys.argv[1] if len(sys.argv) > 1 else "10.22.192.34"
     # args = parse_args()
     # main(args)
-    controller = control.Rover(args.ip)
+    controller = control.Rover(ip)
     controller_thread = threading.Thread(target=controller.run)
     controller_thread.daemon = True
     controller_thread.start()
 
-    camera_thread = threading.Thread(target=mainCamera, args=(args,))
+    camera_thread = threading.Thread(target=mainCamera, args=(ip,))
     camera_thread.daemon = True
     camera_thread.start()
-    
-    print("Streaming to IP address:", args.ip)
 
     try:
         while True:
